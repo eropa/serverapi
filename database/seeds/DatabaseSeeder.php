44cddc28a2605f545b18bf41d7078085e13dc93c
Lutsenko Igor <eropa@live.ru>
2019-08-21 23:34:15 +0300

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        for($i=1;$i<1100;$i++){
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => bcrypt('password'),
+            ]);
+        }
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/*
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+*/
+
+Route::get('/person',function (Request $request){
+
+    try{
+        $data=$request->all();
+        $api_key=$data['api_key'];
+        $confKey=env('APP_KEY');
+
+        if($confKey==$api_key){
+            $dataReturn=\App\User::all();
+            return $dataReturn;
+        }
+        abort(404);
+    }catch (Exception $e) {
+        abort(404);
+    }
+});
